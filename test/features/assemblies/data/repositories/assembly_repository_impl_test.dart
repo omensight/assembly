0b97ad417872bf5a28a26176c21572b346a0b1c1
Mijael Viricochea <mijael.viricochea@outlook.com>
2025-03-29 19:08:24 -0400

    fix(assemblies): Fix assembly repository tests with updated entities record mock

--- test/features/assemblies/data/repositories/assembly_repository_impl_test.dart
+++ test/features/assemblies/data/repositories/assembly_repository_impl_test.dart
@@ -1,9 +1,14 @@
+import 'package:assembly/core/data/database/assembly_database.dart';
+import 'package:assembly/core/data/datasources/updated_entities_record_local_data_source.dart';
 import 'package:assembly/features/assemblies/data/repositories/assembly_repository_impl.dart';
 import 'package:assembly/features/assemblies/domain/entities/assembly.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 void main() {
+  setUpAll(() {
+    registerFallbackValue(UpdatedEntitiesRecord(id: 0));
+  });
   test(
     'Test that the repository is fetching the data in the database',
     () async {
@@ -11,6 +16,9 @@ void main() {
           MockAssemblyLocalDataSource();
       AssemblyRemoteDataSource assemblyRemoteDataSource =
           MockAssemblyRemoteDataSource();
+      UpdatedEntitiesRecordLocalDataSource
+      updatedEntitiesRecordLocalDataSource =
+          MockUpdatedEntitiesRecordLocalDataSource();
       final assemblies = [
         Assembly(
           id: '1',
@@ -25,16 +33,29 @@ void main() {
         () => assemblyLocalDataSource.cacheAssemblies(any()),
       ).thenAnswer((invocation) => Future.value());
       when(
-        () => assemblyRemoteDataSource.getUserAssemblies(),
+        () => assemblyRemoteDataSource.getUserAssemblies(any()),
       ).thenAnswer((invocation) => Future(() => assemblies));
+      when(
+        () => updatedEntitiesRecordLocalDataSource.getUpdatedEntityRecord(),
+      ).thenAnswer(
+        (invocation) => Future(
+          () => UpdatedEntitiesRecord(id: 0, assemblies: DateTime.now()),
+        ),
+      );
+      when(
+        () => updatedEntitiesRecordLocalDataSource.updateRecord(any()),
+      ).thenAnswer((invocation) => Future.value());
+
       final repository = AssemblyRepositoryImpl(
         assemblyLocalDataSource: assemblyLocalDataSource,
         assemblyRemoteDataSource: assemblyRemoteDataSource,
+        updatedEntitiesRecordLocalDataSource:
+            updatedEntitiesRecordLocalDataSource,
       );
       await repository.fetchUserAssemblies();
       // Assert
       verify(() => assemblyLocalDataSource.cacheAssemblies(any())).called(1);
-      verify(() => assemblyRemoteDataSource.getUserAssemblies()).called(1);
+      verify(() => assemblyRemoteDataSource.getUserAssemblies(any())).called(1);
       verifyNoMoreInteractions(assemblyLocalDataSource);
       verifyNoMoreInteractions(assemblyRemoteDataSource);
     },
@@ -46,3 +67,6 @@ class MockAssemblyLocalDataSource extends Mock
 
 class MockAssemblyRemoteDataSource extends Mock
     implements AssemblyRemoteDataSource {}
+
+class MockUpdatedEntitiesRecordLocalDataSource extends Mock
+    implements UpdatedEntitiesRecordLocalDataSource {}
