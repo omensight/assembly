b73521873761780cd2b551482204d9a88b1c852f
Mijael Viricochea <mijael.viricochea@outlook.com>
2025-04-30 18:37:14 -0400

    refactor(assemblies): Rename create assignment settings page

--- lib/features/assemblies/routes.dart
+++ lib/features/assemblies/routes.dart
@@ -3,7 +3,7 @@ import 'package:assembly/features/assemblies/presentation/pages/assembly_joining
 import 'package:assembly/features/assemblies/presentation/pages/assembly_members_page/assembly_members_page.dart';
 import 'package:assembly/features/assemblies/presentation/pages/assignment_detail_page/assignment_detail_page.dart';
 import 'package:assembly/features/assemblies/presentation/pages/create_assembly_page/create_assembly_page.dart';
-import 'package:assembly/features/assemblies/presentation/pages/create_assignment/add_assignment_details_page/add_assignment_details_page.dart';
+import 'package:assembly/features/assemblies/presentation/pages/create_assignment/create_assignment_settings_page/create_assignment_settings_page.dart';
 import 'package:assembly/features/assemblies/presentation/pages/create_assignment/create_assignment_page/create_assignment_page.dart';
 import 'package:assembly/features/assemblies/presentation/pages/edit_assembly_page/edit_assembly_page.dart';
 import 'package:assembly/features/assemblies/presentation/pages/join_requests_list_page/join_requests_list_page.dart';
